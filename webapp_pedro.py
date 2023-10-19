9b699c34d10e2ce01dc24311786dbdc12c2dc3ee
Bruno Donato <bruno.sousadonato@gmail.com>
2023-10-19 13:40:59 -0300

    Adição de similaridade de cosseno e funcoes

--- webapp_pedro.py
+++ webapp_pedro.py
@@ -32,50 +32,7 @@ quali_metod = '''
             baixa qualidade metodológica, e os com nota 7 ou mais foram considerados de alta qualidade 
             metodológica.
             '''
-
-# def graf_linha_tempo(df1=None, nome=None):
-#     fig = px.line(
-#         data_frame=df1, x="ano", y="quantidade", color="tipo estudo", 
-#         title=f'{nome} ({df1["ano"].min()} - 2022): {df1["quantidade"].sum()} estudos'
-#     )
-#     fig.update_layout(title_font=dict(size=18), xaxis_title="", 
-#                       yaxis_title="Quantidade", legend_title='Tipo de estudo', 
-#                       legend=dict(y=0.87, x=0.0775),  width=810, 
-#                       height=500, font=dict(size=13))
-#     return st.plotly_chart(fig, theme='streamlit', use_container_width=True)
-
-# def histograma(df2=None, nome=None):
-#     fig2 = px.histogram(
-#         data_frame=df2, x="escala pedro", histnorm='percent', 
-#         title=f'{nome} ({df2["ano"].min()} - 2022): Qualidade de {df2.shape[0]} ensaios clínicos'
-#     )
-#     fig2.update_layout(title_font=dict(size=18), 
-#                        xaxis_title="Pontuação na Escala PEDro",
-#                        yaxis_title="Porcentagem (%)", width=810, height=500, 
-#                        font=dict(size=13))
-#     return st.plotly_chart(fig2, theme='streamlit', use_container_width=True)
-
-# def bar_quali(df3=None, nome=None):
-#     fig3 = px.bar(df3, x='decada', y='quantidade', color='qualidade', 
-#                   color_discrete_sequence=["red", "blue"], 
-#                   title=f"{nome}: Qualidade dos ensaios clínicos")
-#     fig3.update_layout(title_font=dict(size=18), legend_title='', 
-#                        legend=dict(y=0.78, x=0.085), xaxis_title="Década", 
-#                        yaxis_title="Quantidade", width=810, height=500, 
-#                        font=dict(size=13))
-#     return st.plotly_chart(fig3, theme='streamlit', use_container_width=True)
-
-# def linha_quali_quant(df4=None, df1=None, nome=None):
-#     fig4 = make_subplots(rows=2, cols=1,
-#                          subplot_titles=("Quantidade por década","Nota Escala PEDro (Mediana)"))
-#     fig4.append_trace(go.Scatter(x=df4['decada'], y=df4['quantidade'],
-#                                  ), row=1, col=1)
-#     fig4.append_trace(go.Scatter(x=df4['decada'], y=df4['escala pedro'],
-#                                  ), row=2, col=1)
-#     fig4.update_layout(height=500, width=810, showlegend=False, 
-#                        title_text=f"{nome} ({df1['ano'].min()} - 2022)")
-#     return st.plotly_chart(fig4, theme='streamlit', use_container_width=True)
-
+            
 titulo = "<div align='center'><h1><b>Ciência e Fisioterapia Ortopédica</b></h1></div>"    
 st.write(titulo, unsafe_allow_html=True)
 st.write(
